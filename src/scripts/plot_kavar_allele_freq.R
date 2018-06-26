79f948f8656dd88f94f69e71937aacd2621063cc
bmennis <tuc64916@temple.edu>
2018-06-26 09:38:11 -0400

    data/ *~ *# src/rules/.snakemake

--- src/scripts/plot_kavar_allele_freq.R
+++ src/scripts/plot_kavar_allele_freq.R
@@ -1,6 +1,9 @@
 library(ggplot2)
-          
-d <- read.delim("data/raw/Kaviar_subset_allele_frq.csv", header=TRUE, sep='\t')
+args <- commandArgs(trailingOnly = TRUE)
+dat_file = args[1]
+plot_file = args[2
+
+d <- read.delim(dat_file, header=TRUE, sep='\t')
 p <- ggplot(data=d) + geom_violin(aes(x=Sources, y=log(Allele.Frequency,10))) + theme_bw(base_size=18) + xlab("Sources") + ylab("Allele Frequencies")
-ggsave('allele_plot.png', p)
+ggsave(plot_file, p)
 
