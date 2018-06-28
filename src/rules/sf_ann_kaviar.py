b147a27623af15b2ce3cf6ee4f9df3e785042285
bmennis <tuc64916@temple.edu>
2018-06-28 13:41:33 -0400

    Update kaviar annotation file intersection rules to pull proper files from directories to intersect with vcf files.

--- src/rules/sf_ann_kaviar.py
+++ src/rules/sf_ann_kaviar.py
@@ -18,32 +18,33 @@ rule filterIlluminaOnlySources:
 rule intersectInitialDifficultFiles:
     input:  vcf=DATA + 'interim/kaviar_subsets/{subset}.vcf',
             bed='/mnt/isilon/cbmi/variome/perry/projects/sarmadi/benchmarking-tools/resources/stratification-bed-files/{subset2}/{subset3}.bed.gz'
-    output: DATA + 'interim/kaviar_subsets/{subset}/initial_files_intersections/{subset}_{subset3}.intr'
+    output: DATA + 'interim/kaviar_subsets/{subset}/initial_files_intersections/{subset}_{subset2}__{subset3}.intr'
     shell:  'bedtools intersect -wb -a {input.vcf} -b {input.bed} > {output}'
 
-#rule all:
-#    input: expand(DATA + 'interim/kaviar_subsets/{subset}/initial_files_intersections/{subset}_{subset3}.intr', subset=('cgi_only_sources','illumina_only_sources_filtered','cgi_illumina_sources'), subset2=('mappability','SegmentalDuplications'), subset3=('lowmappabilityall','segdupall'))
+rule all:
+    input: expand(DATA + 'interim/kaviar_subsets/{subset}/initial_files_intersections/{subset}_{subset4}.intr', \
+                  subset=('cgi_only_sources','illumina_only_sources_filtered','cgi_illumina_sources'), \
+                  subset4=('mappability__lowmappabilityall','SegmentalDuplications__segdupall'))
 
 rule intersectFiles:
     input: vcf=DATA + 'interim/kaviar_subsets/{subset}.vcf',
            bed='/mnt/isilon/cbmi/variome/perry/projects/sarmadi/ahmad_exomeizer/data/interim/sql_result.status.bed'
-    output: DATA + 'interim/kaviar_subsets/{subset}/{subset}_hgmd.intr'
+    output: DATA + 'interim/kaviar_subsets/{subset}/{subset}_sql_result.status.intr'
     shell: 'bedtools intersect -wb -a {input.vcf} -b {input.bed} > {output}'
 
 rule intersectFilesAll:
-    input: expand(DATA + 'interim/kaviar_subsets/{subset}/{subset}_hgmd.intr', subset=('cgi_only_sources','illumina_only_sources_filtered','cgi_illumina_sources'))
+    input: expand(DATA + 'interim/kaviar_subsets/{subset}/{subset}_sql_result.status.intr', subset=('cgi_only_sources','illumina_only_sources_filtered','cgi_illumina_sources'))
 
-# rule intersectDifficultByFile:
-#     input: vcf=DATA + 'raw/{subset}.vcf',
-# 	   bed='/mnt/isilon/cbmi/variome/perry/projects/sarmadi/ahmad_exomeizer/data/interim/difficult/by_file/{subset2}/{subset3}.bed'
-#     output: DATA + 'interim/{subset}/{subset2}_{subset3}.intr'
-#     shell: 'bedtools intersect -wb -a {input.vcf} -b {input.bed} > {output}'
+rule intersectDifficultByFile:
+    input: vcf=DATA + 'raw/{subset}.vcf',
+	   bed='/mnt/isilon/cbmi/variome/perry/projects/sarmadi/ahmad_exomeizer/data/interim/difficult/by_file/{subset2}/{subset3}.bed'
+    output: DATA + 'interim/{subset}/{subset}_{subset2}__{subset3}.intr'
+    shell: 'bedtools intersect -wb -a {input.vcf} -b {input.bed} > {output}'
 
-# rule intersectAllDifficultByFile:
-#     input: expand(DATA + 'interim/{subset}/{subset}_{subset3}.intr', \
-#                   subset=('cgi_only_sources','illumina_only_sources_filtered','cgi_illumina_sources'),
-#                   subset2=('FunctionalTechnicallyDifficultRegions','GCcontent','LowComplexity','mappability','SegmentalDuplications'),
-#                   subset3=('BadPromoters_gb-2013-14-5-r51-s1.sql_result.status.difficult','human_g1k_v37_l100_gclt25orgt65_slop50.sql_result.status.difficult','AllRepeats_gt95percidentity_slop5.sql_result.status.difficult','lowmappabilityall.sql_result.status.difficult','segdupall.sql_result.status.difficult'))
+rule intersectAllDifficultByFile:
+    input: expand(DATA + 'interim/{subset}/{subset}_{subset4}.intr', \
+                  subset=('cgi_only_sources','illumina_only_sources_filtered','cgi_illumina_sources'), \
+                  subset4=('FunctionalTechnicallyDifficultRegions__BadPromoters_gb-2013-14-5-r51-s1.sql_result.status.difficult','GCcontent__human_g1k_v37_l100_gclt25orgt65_slop50.sql_result.status.difficult','LowComplexity__AllRepeats_gt95percidentity_slop5.sql_result.status.difficult','mappability__lowmappabilityall.sql_result.status.difficult','SegmentalDuplications__segdupall.sql_result.status.difficult'))
 
 rule sortAlleleFrequency:
     input:  expand(DATA + 'interim/kaviar_subsets/{subset}.vcf', subset=('cgi_only_sources','cgi_illumina_sources','illumina_only_sources_filtered'))
@@ -52,11 +53,11 @@ rule sortAlleleFrequency:
         for afile in input:
             shell('python {SCRIPTS}vcf_sort_allele.py {afile} {output}')
 
-# rule sortAlleleFrequencyAll:
-#     input: expand(DATA + 'raw/Kaviar_subset_allele_frq.csv', subset=('cgi_only_sources','cgi_illumina_sources','illumina_only_sources')
+rule sortAlleleFrequencyAll:
+    input: expand(DATA + 'raw/Kaviar_subset_allele_frq.csv', subset=('cgi_only_sources','cgi_illumina_sources','illumina_only_sources')
 
 # rule sortAlleleFrequencyKaviar:
-#     input: vcf=DATA + '/mnt/isilon/cbmi/variome/bin/gemini/data/gemini_data/Kaviar-160204-Public-hg19.vt.vcf.gz',
-#     output: DATA + 'raw/Kaviar_allele_frq.csv'
-#     shell: 'python src/scripts/vcf_sort_allele.py {input.vcf} {output.csv}'
+#    input: vcf=DATA + '/mnt/isilon/cbmi/variome/bin/gemini/data/gemini_data/Kaviar-160204-Public-hg19.vt.vcf.gz',
+#    output: DATA + 'raw/Kaviar_allele_frq.csv'
+#    shell: 'python src/scripts/vcf_sort_allele.py {input.vcf} {output.csv}'
 
